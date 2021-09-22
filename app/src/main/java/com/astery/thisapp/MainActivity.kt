83790f42397a04b2050d46ca9b8a32411a7a9567
Voronskaya Darya <56038397+VoronDar@users.noreply.github.com>
2021-09-22 08:14:32 +0300

    added fragments

--- app/src/main/java/com/astery/thisapp/MainActivity.kt
+++ app/src/main/java/com/astery/thisapp/MainActivity.kt
@@ -1,23 +1,76 @@
 package com.astery.thisapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var currentFragment: TFragment
+
+    private lateinit var logoutMenu: Menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-        //setContentView(R.layout.activity)
-        setContentView(R.layout.fragment_main)
-        //setSupportActionBar(findViewById(R.id.toolbar))
+        setContentView(R.layout.activity)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        pushFragment()
+
+    }
+
+
+    private fun pushFragment(){
+        currentFragment = LoginFragment()
+        fragmentManager = supportFragmentManager
+        val ft = fragmentManager.beginTransaction()
+        ft.add(R.id.fragment_container, currentFragment)
+        ft.commit()
+
+        showLogout(false)
+
+    }
+
+    fun move(fragment: TFragment) {
+        fragment.setTransition(false)
+        currentFragment.setTransition(true)
+
+        val ft = fragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, fragment)
+        ft.commit()
+
+        currentFragment = fragment
 
 
     }
+
+    fun showLogout(show:Boolean){
+        if (::logoutMenu.isInitialized)
+            this.logoutMenu.findItem(R.id.action_logout).isVisible = show
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_logout -> {
+                move(LoginFragment())
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu, menu)
+        this.logoutMenu = menu
+        showLogout(false)
         return true
     }
 }
